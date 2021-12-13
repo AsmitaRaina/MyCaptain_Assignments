bad8b259014b82b75c47dd486b2cfa1b2d5d2ef5
AsmitaRaina <95634515+AsmitaRaina@users.noreply.github.com>
2021-12-14 00:10:13 +0530

    Update function_1.py

--- function_1.py
+++ function_1.py
@@ -1,4 +1,6 @@
+
 d={}
+d1={}
 def most_frequent(string):
     for i in string:
         c=1
@@ -6,7 +8,11 @@ def most_frequent(string):
             d[i]=c
         else:
             d[i]=d[i]+1
-    print(d)
-   
+    key=list(d.keys())
+    val=sorted(list(d.values()), reverse=True)
+    for j in val:
+        d1[key[val.index(j)]]=j
+    print(d1)
+    
 string=input("Enter word")
 most_frequent(string)
